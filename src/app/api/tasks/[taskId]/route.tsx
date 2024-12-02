c43e31ccc2dfde25b2f22a3bf1c59b32426d6a15
Iman Maulana <maulanaiman902@gmail.com>
2024-12-02 21:22:34 +0700

    feat: add functionality to update task by id

--- src/app/api/tasks/[taskId]/route.tsx
+++ src/app/api/tasks/[taskId]/route.tsx
@@ -156,7 +156,7 @@ export async function PATCH(
     });
 
     return NextResponse.json({
-      message: 'Task updated successfully.',
+      message: 'Task updated successfully. 📝',
       data: task,
     });
   } catch (error) {
@@ -174,7 +174,7 @@ export async function PATCH(
     console.error('Error updating task:', error);
     return NextResponse.json(
       {
-        message: 'Failed to update task.',
+        message: 'Failed to update task. 😞',
       },
       { status: 500 }
     );
---
+++ src/components/fragments/modal-edit.tsx
@@ -0,0 +1,173 @@
+import { Spinner, useDisclosure } from '@nextui-org/react';
+import { Button } from '@nextui-org/button';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '@nextui-org/modal';
+import { Select, SelectItem } from '@nextui-org/select';
+import { Input } from '@nextui-org/input';
+import { Edit2 } from 'lucide-react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { api } from '@/lib/axios';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+interface ModalEditProps {
+  id: number;
+}
+
+export default function ModalEdit({ id }: ModalEditProps) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [value, setValue] = useState('');
+  const [status, setStatus] = useState(false);
+  const queryClient = useQueryClient();
+
+  const {
+    data: task,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['task', id],
+    queryFn: async () => {
+      const response = await api.get(`/api/tasks/${id}`);
+      return response.data;
+    },
+    enabled: shouldFetch,
+    staleTime: 5 * 60 * 1000,
+  });
+
+  const { mutate: updateTask } = useMutation({
+    mutationFn: async ({
+      id,
+      payload,
+    }: {
+      id: number;
+      payload: { taskName: string; status: boolean };
+    }) => {
+      const response = await api.put(`/api/tasks/${id}`, payload);
+      return response.data;
+    },
+    onMutate: () => {
+      toast.loading('Deleting task...', {
+        position: 'top-center',
+        autoClose: 3000,
+        theme: 'light',
+      });
+    },
+    onSuccess: (data) => {
+      toast.dismiss();
+      toast.success(data.message, {
+        position: 'top-center',
+        autoClose: 3000,
+        theme: 'light',
+      });
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+    },
+    onError: (error) => {
+      toast.dismiss();
+      toast.error((error as Error).message, {
+        position: 'top-center',
+        autoClose: 3000,
+        theme: 'light',
+      });
+    },
+  });
+
+  const handleOpen = () => {
+    setShouldFetch(true);
+    onOpen();
+  };
+
+  const handleSubmit = () => {
+    updateTask({ id, payload: { taskName: value, status } });
+    onOpenChange();
+  };
+
+  useEffect(() => {
+    if (task) {
+      setValue(task.data.taskName);
+      setStatus(task.data.isCompleted);
+    }
+  }, [task]);
+
+  const options = [
+    { key: 'true', label: 'Completed' },
+    { key: 'false', label: 'In Progress' },
+  ];
+
+  return (
+    <>
+      <Button
+        isIconOnly
+        color='default'
+        variant='faded'
+        aria-label='Edit task'
+        size='sm'
+        onPress={handleOpen}
+      >
+        <Edit2 size={16} color='#333' />
+      </Button>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top-center'>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className='flex flex-col gap-1'>
+                Edit Task
+              </ModalHeader>
+              <ModalBody>
+                {isLoading ? (
+                  <Spinner size='lg' />
+                ) : isError ? (
+                  <p>Error fetching task data. Please try again.</p>
+                ) : (
+                  <>
+                    <Input
+                      autoFocus
+                      label='Task Name'
+                      placeholder='Enter task name'
+                      variant='bordered'
+                      value={value}
+                      onChange={(e) => setValue(e.target.value)}
+                    />
+                    <Select
+                      label='Filter by Status'
+                      size='sm'
+                      selectedKeys={new Set([status.toString()])}
+                      onSelectionChange={(selected) => {
+                        const selectedKey = Array.from(selected)[0] as string;
+                        setStatus(selectedKey === 'true');
+                      }}
+                      classNames={{
+                        trigger:
+                          'w-full md:w-[180px] lg:w-[200px] bg-[#ffffff]',
+                        listboxWrapper: 'w-full md:w-[180px] lg:w-[200px]',
+                      }}
+                    >
+                      {options.map((item) => (
+                        <SelectItem key={item.key} value={item.key}>
+                          {item.label}
+                        </SelectItem>
+                      ))}
+                    </Select>
+                  </>
+                )}
+              </ModalBody>
+              <ModalFooter>
+                <Button color='danger' variant='light' onPress={onClose}>
+                  Close
+                </Button>
+                <Button color='primary' onPress={handleSubmit}>
+                  Save
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
