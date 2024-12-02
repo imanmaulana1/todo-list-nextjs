357b4f333f89869efa63fed3381cd1f1af1ce3f1
Iman Maulana <maulanaiman902@gmail.com>
2024-12-02 11:59:09 +0700

    feat(ui): add dynamic message for empty task states

--- src/components/fragments/no-task-message.tsx
+++ src/components/fragments/no-task-message.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useTasksContext } from '@/hooks/use-tasks';
+
 export default function NoTaskMessage() {
+  const { query } = useTasksContext();
+
   return (
     <section className='container max-w-screen-xl mx-auto my-8'>
       <div className='min-h-[30vh] flex justify-center items-center p-4 rounded-lg'>
-        <h2 className='text-2xl'>You have no tasks! ✨</h2>
+        <h2 className='text-2xl'>
+          {query ? (
+            <>
+              No task found for
+              <span className='font-bold italic'> &quot;{query}&quot;</span>
+            </>
+          ) : (
+            'You have no tasks! ✨'
+          )}
+        </h2>
       </div>
     </section>
   );
